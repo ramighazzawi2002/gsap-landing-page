b195b064879d470ce3dd7512a711dc0588166eb8
ramighazzawi2002 <rami.ghazzawiabed@gmail.com>
2026-03-13 14:58:25 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+import {ScrollTrigger, SplitText} from 'gsap/all';
+import Navbar from "./components/Navbar.tsx";
+import Hero from "./components/Hero.tsx";
+import {Cocktails} from "./components/Cocktails.tsx";
+import About from "./components/About.tsx";
+import Art from "./components/Art.tsx";
+import {Menu} from "./components/Menu.tsx";
+import {Contact} from "./components/Contact.tsx";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+
+    return (
+        <main>
+            <Navbar/>
+            <Hero/>
+            <Cocktails/>
+            <About/>
+            <Art/>
+            <Menu/>
+            <Contact/>
+        </main>
+    )
+}
+
+export default App
---
+++ src/components/Menu.tsx
@@ -0,0 +1,89 @@
+import {allCocktails} from "../constants";
+import {useRef, useState} from "react";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+export const Menu = () => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const totalCocktails = allCocktails.length;
+    const goToSlide = (index: number) => {
+        const newIndex = (index + totalCocktails) % totalCocktails
+        setCurrentIndex(newIndex)
+    }
+
+    const getCocktailAt = (indexOffset: number) => {
+        return allCocktails[(currentIndex + indexOffset + totalCocktails) % totalCocktails]
+    }
+
+    const currentCocktail = getCocktailAt(0);
+    const prevCocktail = getCocktailAt(-1);
+    const nextCocktail = getCocktailAt(1);
+    const contentRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.fromTo("#title", {opacity: 0}, {opacity: 1, duration: 1})
+        gsap.fromTo(".cocktail img", {opacity: 0, xPercent: -100}, {
+            opacity: 1,
+            xPercent: 0,
+            duration: 1,
+            ease: "power1.inOut"
+        })
+        gsap.fromTo(".details h2", {yPercent: 100, opacity: 0}, {
+            yPercent: 0,
+            opacity: 1,
+            ease: "power1.inOut"
+        })
+        gsap.fromTo(".details p", {yPercent: 100, opacity: 0}, {
+            yPercent: 0,
+            opacity: 1,
+            ease: "power1.inOut"
+        })
+    }, [currentIndex])
+    return (
+        <section id={"menu"} aria-labelledby={"menu"}>
+            <img src={"/images/slider-left-leaf.png"} alt={"left-leaf"} id={"m-left-leaf"}/>
+            <img src={"/images/slider-right-leaf.png"} alt={"right-leaf"} id={"m-right-leaf"}/>
+            <h2 id={"menu-heading"} className={"sr-only"}>
+                Cocktail Menu
+            </h2>
+            <nav className={"cocktail-tabs"} aria-labelled={"cocktail-navigation"}>
+                {allCocktails.map((cocktail, index) => {
+                    const isActive = index === currentIndex;
+                    return (
+                        <button key={cocktail.id}
+                                className={`${isActive ? "text-white border-white" : "tet-white/50 border-white/50 "}`}
+                                onClick={() => goToSlide(index)}
+                        >
+                            {cocktail.name}
+                        </button>
+                    )
+                })}
+            </nav>
+            <div className={"content"}>
+                <div className={"arrows"}>
+                    <button className={"text-left"} onClick={() => goToSlide(currentIndex - 1)}>
+                        <span>{prevCocktail.name}</span>
+                        <img src={"/images/right-arrow.png"} alt={"right-arrow"} aria-hidden={true}/>
+                    </button>
+                    <button className={"text-left"} onClick={() => goToSlide(currentIndex + 1)}>
+                        <span>{nextCocktail.name}</span>
+                        <img src={"/images/left-arrow.png"} alt={"left-arrow"} aria-hidden={true}/>
+                    </button>
+                </div>
+                <div className={"cocktail"}>
+                    <img src={currentCocktail.image} className={"object-contain"} alt={"cocktail"}/>
+                </div>
+                <div className="recipe">
+                    <div className="info" ref={contentRef}>
+                        <p>Recipe for:</p>
+                        <p id={"title"}>{currentCocktail.name}</p>
+                    </div>
+                    <div className="details">
+                        <h2>{currentCocktail.title}</h2>
+                        <p>{currentCocktail.description}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
